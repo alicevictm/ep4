27afc5ebc8fd46a13f43936657c13cf888be444f
AaAA\aaaAaa <127904281+alicevictm@users.noreply.github.com>
2023-10-25 08:48:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ function botaozinho_resposta() {
     var respostaUsuario2 = document.querySelector('input[name="resposta2"]:checked');
     var respostaUsuario3 = document.querySelector('input[name="resposta3"]:checked');
     var respostaUsuario4 = document.querySelector('input[name="resposta4"]:checked');
+    var respostaUsuario5 = document.querySelector('input[name="resposta"]:checked');
+    var respostaUsuario6 = document.querySelector('input[name="resposta"]:checked');
+    var respostaUsuario7 = document.querySelector('input[name="resposta"]:checked');
+    var respostaUsuario8 = document.querySelector('input[name="resposta"]:checked');
+    var respostaUsuario9 = document.querySelector('input[name="resposta"]:checked');
+    var respostaUsuario10 = document.querySelector('input[name="resposta"]:checked');
 
-
-    if(respostaUsuario1 && respostaUsuario2 && respostaUsuario3 && respostaUsuario4){
+    if(respostaUsuario1 && respostaUsuario2 && respostaUsuario3 && respostaUsuario4 && respostaUsuario5 && respostaUsuario5 && respostaUsuario6 && respostaUsuario7 && respostaUsuario8 && respostaUsuario9 && respostaUsuario10 ){
         if(respostaUsuario1.value === "correto"){
             document.getElementById("resultado_1").innerHTML = "acertou!";
         }
@@ -29,6 +34,42 @@ function botaozinho_resposta() {
         }
         else{
             document.getElementById("resultado_4").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_5").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_5").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_6").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_6").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_7").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_7").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_8").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_8").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_9").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_9").innerHTML = "ERROU!!!";
+        }
+        if(respostaUsuario4.value === "correto"){
+            document.getElementById("resultado_10").innerHTML = "acertou";
+        }
+        else{
+            document.getElementById("resultado_10").innerHTML = "ERROU!!!";
         }
     }
     else{
